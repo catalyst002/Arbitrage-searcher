32a96c25ca5864d67289746327f726353e03e584
catalyst002 <108342453+catalyst002@users.noreply.github.com>
2024-03-28 01:56:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,100 @@
 import asyncio
 import json
-import time
-import aiohttp
-import decimal
 import sqlite3 as sl
+from decimal import Decimal, getcontext
+import aiohttp
 
-decimal.getcontext().prec = 3
+# Set decimal precision
+getcontext().prec = 3
 
+# Initialize database connection
 db = sl.connect('test.db')
 
 
-def insertdata(coinname, xsymbol, ysymbol, xchangename, ychangename, diffcheck):
+def setup_database():
     with db:
-        db.execute(f"""
-					CREATE TABLE IF NOT EXISTS deals (
-					xsymbol TEXT,
-					ysymbol TEXT,
-					xchangename TEXT,
-					ychangename TEXT,
-					diff int
-					);
-				""")
-        sql = f'INSERT INTO deals (xsymbol, ysymbol, xchangename, ychangename, diff) VALUES (?, ?, ?,?,?)'
-        data = [
-            (xsymbol, ysymbol, xchangename, ychangename, diffcheck)
-        ]
-        db.executemany(sql, data)
+        db.execute("""
+            CREATE TABLE IF NOT EXISTS deals (
+                xsymbol TEXT,
+                ysymbol TEXT,
+                xchangename TEXT,
+                ychangename TEXT,
+                diff REAL
+            );
+        """)
 
 
-def proxypool(amount_of_proxies):
-    global proxycounter
-    if proxycounter < amount_of_proxies:
-        proxycounter += 1
-    else:
-        proxycounter = 0
+def insert_data(coinname, xsymbol, ysymbol, xchangename, ychangename, diffcheck):
+    with db:
+        sql = 'INSERT INTO deals (xsymbol, ysymbol, xchangename, ychangename, diff) VALUES (?, ?, ?, ?, ?)'
+        db.execute(sql, (xsymbol, ysymbol, xchangename, ychangename, diffcheck))
 
 
-proxycounter = 0
+def get_proxy(proxies, counter):
+    return proxies[counter % len(proxies)]
+
+
+async def fetch_coin_data(session, coin, proxy):
+    url = f'https://api.cryptorank.io/v0/coins/{coin}/tickers?includeQuote=false'
+    async with session.get(url, proxy=f"http://{proxy}") as response:
+        return await response.json()
+
+
+async def process_coins(minspread, maxspread, proxies):
+    setup_database()
+
+    with open('coins.txt', 'r') as f, open('proxy.txt', 'r') as p:
+        coins = f.read().splitlines()
+        proxies = p.read().splitlines()
+
+    async with aiohttp.ClientSession() as session:
+        for idx, coin in enumerate(coins):
+            proxy = get_proxy(proxies, idx)
+            jsonresp = await fetch_coin_data(session, coin, proxy)
+            dataarray = jsonresp.get('data', [])
+            await compare_prices(minspread, maxspread, dataarray)
+
+
+async def compare_prices(minspread, maxspread, dataarray):
+    for x in dataarray:
+        for y in dataarray:
+            try:
+                diffcheck = calculate_diffcheck(x, y)
+                if criteria_met(diffcheck, minspread, maxspread, x, y):
+                    insert_data(
+                        x['coinName'], x['symbol'], y['symbol'], x['exchangeName'],
+                        y['exchangeName'], diffcheck
+                    )
+            except (ZeroDivisionError, KeyError):
+                continue
+
+
+def calculate_diffcheck(x, y):
+    xprice, yprice = Decimal(x['usdLast']), Decimal(y['usdLast'])
+    diff = abs(xprice - yprice)
+    percent = (diff / xprice) * 100
+    totalspread = x.get('spread', 0) + y.get('spread', 0)
+    return percent - totalspread
+
+
+def criteria_met(diffcheck, minspread, maxspread, x, y):
+    return (
+        minspread < diffcheck < maxspread and
+        x['exchangeName'] != y['exchangeName'] and
+        x['exchangeName'] in {'Binance', 'Kucoin', 'OKX', 'Huobi', 'MEXC Global'} and
+        y['exchangeGroup'] == 'dex' and
+        y['exchangeName'] != 'Raydium' and
+        x['to'] in {'USDT', 'BUSD', 'USDC'} and
+        y['usdVolume'] > 10000
+    )
 
 
 async def main():
-    minspread = int(input("Input the min spread"))
-    maxspread = int(input("Input the max spread"))
-    amount_of_proxies= int(input("Enter the amount of proxies"))
-    with open('coins.txt', 'r') as f:
-        with open('proxy.txt', 'r') as p:
-            coins = f.read().splitlines()
-            proxies = p.read().splitlines()
-
-            for i in range(0, len(coins)):
-
-                url = f'https://api.cryptorank.io/v0/coins/{coins[i]}/tickers?includeQuote=false'
-
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url, proxy=f"http://{proxies[proxycounter]}") as response:
-                        proxypool(amount_of_proxies)
-                        resp = await response.text()
-                        jsonresp = json.loads(resp)
-                        dataarray = jsonresp['data']
-                        for x in dataarray:
-                            for y in dataarray:
-                                xprice = x['usdLast']
-                                yprice = y['usdLast']
-                                xspread = x.get('spread', 0)
-                                yspread = y.get('spread', 0)
-                                diff = abs(xprice - yprice)
-                                print(x['coinName'])
-                                try:
-                                    percent = diff / xprice * 100
-                                except ZeroDivisionError:
-                                    continue
-                                totalspread = xspread + yspread
-                                diffcheck = percent - totalspread
-                                xname = x['exchangeName']
-                                yname = y['exchangeName']
-                                xvolume = x['usdVolume']
-                                yvolume = y['usdVolume']
-                                if (diffcheck > minspread and diffcheck < maxspread) and (xname != yname) and (xname == 'Binance' or xname == 'Kucoin' or xname == 'OKX' or xname == 'Huobi' or xname == 'MEXC Global') and (y['exchangeGroup'] == 'dex') and (yname != 'Raydium') and (x['to'] == 'USDT' or x['to'] == 'BUSD' or x['to'] == 'USDC') and (yvolume > 10000):
-                                    print(x['exchangeName'], x['usdLast'],
-                                          y['exchangeName'], y['usdLast'], round(
-                                        diffcheck, 3), y['url'])
-                                    insertdata(x['coinName'],
-                                               x['symbol'], y['symbol'], x['exchangeName'],
-                                               y['exchangeName'],  diffcheck)
+    minspread = int(input("Input the min spread: "))
+    maxspread = int(input("Input the max spread: "))
+    await process_coins(minspread, maxspread, proxies)
 
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
